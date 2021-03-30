edcebd54dcb6fec87fb79f3f7f0bfc9b771cf93d
Ferhad Mislim <User@XPS159560>
2021-03-30 01:58:30 -0400

    Card.java updated added data field Value and Suit encapsulated

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -13,15 +13,43 @@ package ca.sheridancollege.project;
  * @author Megha Patel
  * @author Ferhad Mislim
  */
-public abstract class Card {
+public class Card {
     //default modifier for child classes
+    private Suit suit;
+    private Value value;
+    public Card (Value cardValue, Suit suit)
+   {
+      this.value = cardValue;
+      this.suit = suit;
+   }
 
+   public Suit getSuit ()
+   {
+      return suit;
+   }
+
+   public void setSuit (Suit suit)
+   {
+      this.suit = suit;
+   }
+
+   public Value getValue ()
+   {
+      return value;
+   }
+
+   public void setValue (Value value)
+   {
+      this.value = value;
+   }
     /**
      * Students should implement this method for their specific children classes
      *
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
     @Override
-    public abstract String toString();
+    public String toString(){
+        return String.format("Suit:%s Value:%s",getSuit(),getValue());
+    }
 
 }
