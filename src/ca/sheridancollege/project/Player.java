f6c7b3ff2fcff74b41d4b538f12ce2ebc7e322a7
Ferhad Mislim <User@XPS159560>
2021-03-30 02:03:05 -0400

    Player.java modified

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -4,6 +4,8 @@
  * Add your name as an author and the date!
  */
 package ca.sheridancollege.project;
+import ca.sheridancollege.project.Card;
+import java.util.ArrayList;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
@@ -13,10 +15,10 @@ package ca.sheridancollege.project;
  * @author Megha Patel
  * @author Ferhad Mislim
  */
-public abstract class Player {
+public class Player {
 
     private String name; //the unique name for this player
-
+    private ArrayList<Card> hand;// create a hand of card
     /**
      * A constructor that allows you to set the player's unique ID
      *
@@ -24,6 +26,7 @@ public abstract class Player {
      */
     public Player(String name) {
         this.name = name;
+        this.hand = new ArrayList<Card>();
     }
 
     /**
@@ -43,9 +46,23 @@ public abstract class Player {
     }
 
     /**
+     * @return the hand
+     */
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    /**
+     * @param hand the hand to set
+     */
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    /**
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
      * with logic to play your game.
      */
-    public abstract void play();
+    /*public abstract void play();*/
 
 }
